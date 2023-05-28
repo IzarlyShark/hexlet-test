5d43f1e7d3a69265b62c99e9f0dfa8ba33e5876b
IzarlyShark <115780892+IzarlyShark@users.noreply.github.com>
2023-05-28 13:23:17 +0300

    step 1, 2, 3 , 4

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 export default function solution(content){
   // BEGIN
+console.log(content);
+const dataContent = {}
 
+dataContent.arr = content.trim().split('\n').slice(1).map(item => item.split(','))
+dataContent.count = dataContent.arr.length
+dataContent.cities = Array.from(new Set(dataContent.arr.map(item => item[7]))).join(', ')
+dataContent.humidity = {
+  min: Math.min(...dataContent.arr.map(item => item[3])),
+  max: Math.max(...dataContent.arr.map(item => item[3])),
+}
+
+const maxTemperture = Math.max(...dataContent.arr.map(item => item[1]));
+const HottestDay = dataContent.arr.filter((item) => item[1] == maxTemperture)[0]
+dataContent.HottestDay = HottestDay[0] + ' ' + HottestDay[7]
+
+console.log(dataContent)
   // END
 }
